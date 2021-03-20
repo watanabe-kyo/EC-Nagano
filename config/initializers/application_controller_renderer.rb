71bb9b7b1fd7ef8ca72711da3285b87a98d069c6
watanabe-kyo <watanabe.kyo.infratop@gmail.com>
2021-03-20 04:17:17 +0000

    ３．商品登録完成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8f31b5b30a04079d6fa8a90d28564cbe8c75cff83a0ef1fa01ae62a9c2dec995b9f2beaee03c9cf777ffe188278366b1038809edff926d8f8b54f21523e130bf'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_105542) do
+ActiveRecord::Schema.define(version: 2021_03_20_013754) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,24 @@ ActiveRecord::Schema.define(version: 2021_03_19_105542) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
